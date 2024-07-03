d2da091d715891e51f806c7cf75ecb2476fa7290
Akshay sharma <akshaysharma5432@gmail.com>
2024-07-02 21:15:51 -0400

    bold the sign in required text

--- src/components/SignInList.js
+++ src/components/SignInList.js
@@ -7,11 +7,11 @@ const SignInList = () => {
       <div className="learn-more-section">
         <h2 className="learn-more-title">Want to learn more?</h2>
         <p className="result-para">
-          Enter your email address below and my Discover Your Intuitive Style
-          Report will be sent directly to your inbox. This report will give you
-          more information on how you can create a stronger connection to your
-          intuition. You will also receive information on upcoming events, blog
-          posts, articles, and offerings.
+          Enter your email address below and my{" "}
+          <b>Discover Your Intuitive Style Report</b> will be sent directly to
+          your inbox. This report will give you more information on how you can
+          create a stronger connection to your intuition. You will also receive
+          information on upcoming events, blog posts, articles, and offerings.
         </p>
         <form>
           <label htmlFor="email">*Email</label>
