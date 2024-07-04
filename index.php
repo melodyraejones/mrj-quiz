d861880afbff5fd30a28460afa02f1ffeb253671
Akshay sharma <akshaysharma5432@gmail.com>
2024-07-04 03:06:10 -0400

    try another fix

--- index.php
+++ index.php
@@ -13,19 +13,14 @@ class MRJQuiz {
     function __construct() {
         add_action('init', array($this, 'adminAssets'));
         add_action('wp_enqueue_scripts', array($this, 'enqueue_frontend_assets'));
-        add_action('admin_menu', array($this, 'add_admin_menu'));
-        add_action('admin_init', array($this, 'settings_init'));
-        add_action('enqueue_block_editor_assets', array($this, 'enqueue_block_editor_assets'));
         add_action('init', array($this, 'create_personality_post_type'));
         add_action('rest_api_init', array($this, 'register_custom_routes'));
     }
 
     function adminAssets() {
-        // Register styles and scripts for the editor
         wp_register_style('quizEditCSS', plugin_dir_url(__FILE__) . 'build/index.css');
         wp_register_script('newBlockType', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
 
-        // Register block type with style and script
         register_block_type('mrjplugin/quiz', array(
             'editor_script' => 'newBlockType',
             'editor_style' => 'quizEditCSS',
@@ -49,54 +44,6 @@ class MRJQuiz {
         <?php return ob_get_clean();
     }
 
-    function add_admin_menu() {
-        add_menu_page(
-            'Personality Types', // Page title
-            'Personality Types', // Menu title
-            'manage_options', // Capability
-            'edit.php?post_type=personality_type', // Menu slug
-            '', // Function, leave empty as it redirects to post type
-            'dashicons-admin-users', // Icon
-            20 // Position
-        );
-    }
-
-    function settings_init() {
-        register_setting('mrjplugin_settings', 'mrjplugin_personality_types');
-
-        add_settings_section(
-            'mrjplugin_settings_section',
-            __('Manage Personality Types', 'mrjplugin'),
-            null,
-            'mrjplugin_settings'
-        );
-
-        add_settings_field(
-            'mrjplugin_personality_types',
-            __('Personality Types', 'mrjplugin'),
-            array($this, 'personality_types_render'),
-            'mrjplugin_settings',
-            'mrjplugin_settings_section'
-        );
-    }
-
-    function personality_types_render() {
-        $options = get_option('mrjplugin_personality_types');
-        ?>
-        <textarea name="mrjplugin_personality_types" rows="10" cols="50" class="large-text code"><?php echo isset($options) ? esc_textarea($options) : ''; ?></textarea>
-        <p class="description">Enter personality types, one per line.</p>
-        <?php
-    }
-
-    function enqueue_block_editor_assets() {
-        $personality_types = get_option('mrjplugin_personality_types', '');
-        $personality_types_array = array_map('trim', explode("\n", $personality_types));
-        $personality_types_array = array_filter($personality_types_array);
-        wp_localize_script('newBlockType', 'mrjplugin_data', array(
-            'personality_types' => $personality_types_array
-        ));
-    }
-
     function create_personality_post_type() {
         register_post_type('personality_type',
             array(
@@ -109,6 +56,8 @@ class MRJQuiz {
                 'show_in_rest' => true,
                 'supports'    => array('title', 'editor', 'thumbnail'),
                 'rewrite'     => array('slug' => 'personality_type'),
+                'menu_icon'   => 'dashicons-admin-users',
+                'menu_position' => 20,
             )
         );
     }
@@ -125,18 +74,12 @@ class MRJQuiz {
         $slug = sanitize_text_field($data['slug']);
         $query_args = array(
             'post_type' => 'personality_type',
-            'meta_query' => array(
-                array(
-                    'key' => '_wp_old_slug',
-                    'value' => $slug,
-                    'compare' => '='
-                )
-            ),
+            'name' => $slug,
             'post_status' => 'publish'
         );
         $query = new WP_Query($query_args);
 
-        error_log('Query Args: ' . print_r($query_args, true)); // Debugging query arguments
+        error_log('Query Args: ' . print_r($query_args, true));
 
         if ($query->have_posts()) {
             $posts = $query->posts;
@@ -149,11 +92,11 @@ class MRJQuiz {
                 'featured_media_url' => get_the_post_thumbnail_url($post->ID, 'full')
             );
 
-            error_log('Post found: ' . print_r($response, true)); // Debugging response
+            error_log('Post found: ' . print_r($response, true));
 
             return rest_ensure_response($response);
         } else {
-            error_log('No post found for slug: ' . $slug); // Debugging no post found
+            error_log('No post found for slug: ' . $slug);
             return new WP_Error('no_post', 'No post found', array('status' => 404));
         }
     }
