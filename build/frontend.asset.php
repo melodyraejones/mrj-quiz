6c05dd205e489dd13053b4bb841ab7b46c03125f
Akshay sharma <akshaysharma5432@gmail.com>
2024-07-04 03:52:37 -0400

    frontend styles

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'c7fb73e8a7e6d30a59db');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'e231d2939794e66ad2d7');
